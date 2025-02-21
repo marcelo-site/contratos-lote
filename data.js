565b17c3146b89c4f8865d891aeb485b7b10f43f
Marcelo <57197501+marcelo-site@users.noreply.github.com>
2025-02-20 22:23:45 -0300

    .

---
+++ data.js
@@ -0,0 +1,87 @@
+export const data = [
+  {
+    name: "Edjane Barbosa da Silva",
+    pay: false,
+    contact: "",
+    descrpition: "Ednice comprou o lote 07 que também foi vendido à Rubem Fernando de Souza, há também um recibo que alega ter feito um adiantamento de parcelas e com isso recebeu um desconto.",
+    documents: [
+      "contrato-edjane.pdf",
+      "recibo-1.pdf",
+    ]
+  },
+  {
+    name: "Maria Edinice Silva Sant'ana",
+    pay: false,
+    contact: "",
+    descrpition: "Alega ter feito um adiantamento e com isso recebeu um desconto, há pouco junto com o marido começou a construir, o marido é um  homem com um tampão no olho chamado Genegrecio ou Antonio de Jó.",
+    documents: [
+      "contrato-ednice.pdf",
+      "recibo-ednice.pdf",
+    ]
+  },
+
+  {
+    name: "Maria Esterffany da Silva Santos",
+    pay: false,
+    contact: "",
+    descrpition: "Comprou o lote 04 mas, aparentemente resolveu com Paulo indo para outro loteamento.",
+    documents: [
+      "contrato-esterffany.pdf",
+    ]
+  },
+  {
+    name: "José Evano de Oliveira",
+    pay: false,
+    contact: "",
+    descrpition: "",
+    documents: [
+      "contrato-evano.pdf",
+    ]
+  },
+  {
+    name: "Maria Aparecida de Lima Santos",
+    pay: false,
+    contact: "",
+    descrpition: "Comprou o lote 19 que Paulo falou estar disponivél para venda.",
+    documents: [
+      "contrato-maria-aparecida.pdf",
+    ]
+  },
+  {
+    name: "Rubem Fernando de Souza",
+    pay: false,
+    contact: "",
+    descrpition: "Comprou o lote 07 que também foi vendido a Ednice",
+    documents: [
+      "contrato-rubem.pdf"
+    ]
+  },
+  {
+    name: "José Lucas de Araújo Silva",
+    pay: false,
+    contact: "",
+    descrpition: "Comprou os lote 2 e 3 mas, no contrato como 8 e 9 ele é Familia de Leonardo",
+    documents: [
+      "contrato-jose-lucas-08.pdf",
+      "contrato-jose-lucas-09.pdf"
+    ]
+  },
+  {
+    name: "Rafael de Araújo Silva",
+    pay: true,
+    contact: "(81) 9889-3196",
+    descrpition: "Professor Rafael comprou um lote que Paulo acordou que estava pago",
+    documents: [
+      "contrato-rafael.pdf"
+    ]
+  },
+  {
+    name: "Maria Alessandra da Silva Lima Alquino",
+    pay: true,
+    contact: "",
+    descrpition: "Maria Alçessandra, casada com Leonardo, comprou um lote que Paulo acordou que estava pago",
+    documents: [
+      "contrato-maria-alessandra.pdf"
+    ]
+  },
+]
---
+++ script.js
@@ -0,0 +1,142 @@
+import { data } from "./data.js"
+const containerEmbed = document.querySelector("#modal-pdf")
+const embed = containerEmbed.querySelector("embed")
+const replyViewDocument = document.querySelector("#reply")
+const table = document.querySelector("table")
+const tbody = table.querySelector("tbody")
+
+function greeting() {
+  const date = new Date()
+  const hrs = date.getHours()
+
+  if (hrs >= 0 && hrs < 12) {
+    return `Olá, bom dia`
+  } else if (hrs >= 12 && hrs < 18) {
+    return `Olá, boa tarde`
+  }
+  else if (hrs >= 18 && hrs < 23) {
+    return `Olá, boa noite`
+  }
+}
+
+function render({
+  name, contact, pay, containerDesc, background
+}) {
+  const tr = document.createElement("tr")
+  tr.classList.add("primary")
+  tr.style.background = background ? "#f8f8f8" : ""
+  const tdName = document.createElement("td")
+  tdName.innerText = name
+  tr.append(tdName)
+
+  const tdPay = document.createElement("td")
+  tdPay.classList.add("pay")
+  tdPay.innerHTML = pay ? '<i class="bi bi-check-circle-fill"></i>' : '<i class="bi bi-x-circle-fill"></i>'
+
+  tdPay.style.color = pay ? "green" : "red"
+
+  tr.append(tdPay)
+
+  const tdContact = document.createElement("td")
+  tdContact.innerHTML = contact ? `<a class="btn-zap" target=_blank href="https://api.whatsapp.com/send?phone=55${contact.replace(/\D/g, "")}&text=${greeting()}">
+<span>${contact}</span>
+<span>
+    <i class="bi bi-whatsapp"></i>
+</span>
+  </a>` : "Sem número"
+  tr.append(tdContact)
+
+  const tdEye = document.createElement("td")
+  tdEye.classList.add("eye")
+
+  tdEye.innerHTML = "<i class='bi bi-file-earmark-text'>"
+
+  tdEye.addEventListener("click", () => {
+    containerDesc.classList.toggle("none")
+  })
+  tr.append(tdEye)
+
+  return tr
+}
+
+function renderInfo({ descrpition, documents }) {
+  const tr = document.createElement("tr")
+  tr.classList.add("container-description", "none")
+  const td = document.createElement("td")
+  td.colSpan = 4
+
+  const containerInfo = document.createElement("div")
+  const containerDesc = document.createElement("div")
+  const divDesc = document.createElement("div")
+
+  const divBtnDocument = document.createElement("div")
+  const buttonDesc = document.createElement("button")
+  const buttonDocument = document.createElement("button")
+
+  buttonDesc.classList.add("active")
+  buttonDesc.innerText = "Notas"
+  containerInfo.append(buttonDesc)
+
+  const divButtons = document.createElement("div")
+  divButtons.classList.add("btn-document")
+  divButtons.classList.add("none")
+
+  buttonDesc.addEventListener("click", ({ target }) => {
+    target.classList.add("active")
+    buttonDocument.classList.remove("active")
+    divButtons.classList.add("none")
+    divDesc.classList.remove("none")
+  })
+
+  buttonDocument.addEventListener("click", ({ target }) => {
+    target.classList.add("active")
+    buttonDesc.classList.remove("active")
+    divButtons.classList.remove("none")
+    divDesc.classList.add("none")
+  })
+
+  buttonDocument.innerText = "Documentos"
+  containerInfo.append(buttonDocument)
+
+  divDesc.innerText = descrpition || "Sem notas"
+  divDesc.classList.add("description")
+  containerDesc.append(divDesc)
+  containerInfo.append(containerDesc)
+
+  documents.forEach(doc => {
+    const documentDiv = document.createElement("div")
+
+    documentDiv.innerHTML = `<span>${doc}</span>
+        <span> <i class="bi bi-download"></i></span>`
+    divBtnDocument.append(documentDiv)
+    documentDiv.addEventListener("click", () => {
+      embed.src = "./assets/documents/" + doc
+      containerEmbed.style.display = ""
+    })
+    divButtons.append(documentDiv);
+  })
+
+  containerInfo.append(divButtons)
+  td.append(containerInfo)
+
+  tr.append(td)
+
+  return tr
+}
+
+replyViewDocument.addEventListener("click", () => {
+  containerEmbed.style.display = "none"
+  embed.src = ""
+});
+
+(function () {
+  data.forEach((item, i) => {
+    const { contact, descrpition, documents, name, pay } = item
+
+    const trInfo = renderInfo({ descrpition, documents })
+    const tr = render({ contact, name, pay, containerDesc: trInfo, background: i % 2 === 0 })
+
+    tbody.append(tr)
+    tbody.append(trInfo)
+  })
+})()
