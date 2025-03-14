b4ced526aef8b6a2959d1cbfeedde37d4935d4ee
Marcelo <57197501+marcelo-site@users.noreply.github.com>
2025-03-13 22:11:14 -0300

    Mudou container Processos add TAB

--- assets/js/cnj.js
+++ assets/js/cnj.js
@@ -1,58 +1,38 @@
 
 import { handleDocs } from "./script.js"
-import { showSheet } from "./bottom-sheet.js"
 import { dataMovements, docsProcess } from "./data-movement.js"
+import { contentSubject } from "./contentSubject.js"
 
-const btnSubject = document.querySelector("#btn-subject");
-const btnDocProcess = document.querySelector("#btn-doc-process");
 const containerSubject = document.querySelector("#subject");
 const containerDocProcess = document.querySelector("#doc-process");
 const containerMovements = document.querySelector("#movements > div");
 const detailsMain = document.querySelectorAll(".details-main");
+const tabcontent = document.querySelectorAll(".tabcontent");
+const tablinks = document.querySelectorAll(".tablinks");
 
-const subject = `DIREITO ADMINISTRATIVO E OUTRAS MATÉRIAS DE DIREITO PÚBLICO - DIREITO CIVIL (899) - Obrigações (7681) - Inadimplemento (7691) - Rescisão / Resolução (10582 - DIREITO CIVIL (899) - Fatos Jurídicos (7947) - Ato / Negócio Jurídico (4701) - Defeito, nulidade ou anulação (4703 - DIREITO PROCESSUAL CIVIL E DO TRABALHO (8826) - Tutela Provisória (9192) - Liminar (9196`
-
-const toggle = (e) => {
-  const { id } = e.target
-
-  if (id === "btn-doc-process") {
-    btnSubject.classList.remove("active");
-    containerSubject.classList.add("none");
-
-    btnDocProcess.classList.add("active");
-    containerDocProcess.classList.remove("none");
-  } else if (id === "btn-subject") {
-    btnSubject.classList.add("active");
-    containerSubject.classList.remove("none");
-
-    btnDocProcess.classList.remove("active");
-    containerDocProcess.classList.add("none");
+function openCity(evt) {
+  let i = 0
+  for (i = 0; i < tabcontent.length; i++) {
+    tablinks[i].classList.remove("active");
   }
-}
+  const open = evt.target.getAttribute('data-open');
 
-btnDocProcess.addEventListener("click", toggle)
+  tabcontent.forEach(tab => {
+    if (tab.id === open) {
+      tab.classList.remove("none");
+    } else {
+      tab.classList.add("none");
+    }
+  });
 
-btnSubject.addEventListener("click", (e) => {
-  toggle(e)
-  e.stopPropagation()
-})
-
-const baseURL = "https://gateway.cloud.pje.jus.br/tpu"
-const icon = `<i class="bi bi-question-circle"></i>`
-
-const getSubject = async (code) => {
-  const res = await fetch(baseURL + `/api/v1/publico/consulta/detalhada/assuntos?codigo=${code}`)
-
-  const json = await res.json()
-
-  return json
+  evt.currentTarget.classList.add("active");
 }
 
 const contentLine = (active) => {
   return `<div>
 <div class="dot ${active ? "active" : ""}"></div>
 <div class="line ${active ? "active" : ""}"></div>
-</div>`
+</div>`;
 }
 
 const contentMovement = ({ date, content, open }) => {
@@ -61,11 +41,6 @@ const contentMovement = ({ date, content, open }) => {
       <summary class="summary" style="width:100%; border: 0; padding: 0;">${date}</summary>
     <p>${content}</p>
       </details`
-    //     `<div class="timeline-content">
-    // <h4>${date}</h4>
-    // <p>${content}</p>
-    // <span></span>
-    // </div>`
   )
 }
 
@@ -73,7 +48,7 @@ const insertMovements = ({ date, content, active }) => {
   const div = document.createElement("div");
   div.classList.add("card");
   div.innerHTML = contentLine(active);
-  div.innerHTML += contentMovement({ date, content, open: !active });
+  div.innerHTML += contentMovement({ date, content, open: false });
   return div
 }
 
@@ -94,41 +69,15 @@ detailsMain.forEach(item => {
 });
 
 const inity = async () => {
-  const subjectSplit = subject.split("-")
-  const arraySubject = Array.from(subjectSplit)
-
-  await Promise.all(arraySubject.map(async (item) => {
-    const div = document.createElement("div")
-    div.innerHTML = `<span>${item}</span>`
-    const code = item.replace(/\D/g, "")
-
-    if (code) {
-      const contentSubject = await getSubject(code)
-      const desc = contentSubject[0].descricao_glossario
-      const norma = contentSubject[0].norma
-      if (desc || norma) {
-        const button = document.createElement("button")
-        button.innerHTML = `<sup>${icon}</sup>`
-
-        button.addEventListener("click", () => {
-          const div = document.createElement("span");
-          if (desc) {
-            div.innerHTML += `<div><span style="font-weight: 600; font-size:20px; margin: 10px 0; display:inline-block;">Explicação: </span><br />${desc}</div>`
-          }
-          if (norma) {
-            div.innerHTML += `<div><span style="font-weight: 600; font-size:20px; margin: 10px 0; display:inline-block;">Norma: </span><br />${norma}</div>`
-          }
-          showSheet(div)
-        })
-
-        div.append(button)
-      }
-    }
-    containerSubject.appendChild(div)
-  }));
+  await contentSubject(containerSubject)
 
   const docs = handleDocs(docsProcess);
-  containerDocProcess.append(docs)
+  containerDocProcess.append(docs);
+
+  tablinks.forEach(tablink => tablink.addEventListener("click", openCity));
 }
 
-inity();
+window.addEventListener("DOMContentLoaded", () => {
+  inity();
+})
+
---
+++ assets/js/contentSubject.js
@@ -0,0 +1,48 @@
+import { showSheet } from "./bottom-sheet.js";
+
+export const subject = `DIREITO ADMINISTRATIVO E OUTRAS MATÉRIAS DE DIREITO PÚBLICO - DIREITO CIVIL (899) - Obrigações (7681) - Inadimplemento (7691) - Rescisão / Resolução (10582 - DIREITO CIVIL (899) - Fatos Jurídicos (7947) - Ato / Negócio Jurídico (4701) - Defeito, nulidade ou anulação (4703 - DIREITO PROCESSUAL CIVIL E DO TRABALHO (8826) - Tutela Provisória (9192) - Liminar (9196`
+
+const baseURL = "https://gateway.cloud.pje.jus.br/tpu"
+const icon = `<i class="bi bi-question-circle"></i>`
+
+const getSubject = async (code) => {
+  const res = await fetch(baseURL + `/api/v1/publico/consulta/detalhada/assuntos?codigo=${code}`);
+
+  const json = await res.json();
+  return json;
+}
+
+export const contentSubject = async (containerSubject) => {
+  const subjectSplit = subject.split("-")
+  const arraySubject = Array.from(subjectSplit)
+
+  await Promise.all(arraySubject.map(async (item) => {
+    const div = document.createElement("div")
+    div.innerHTML = `<span>${item}</span>`
+    const code = item.replace(/\D/g, "")
+
+    if (code) {
+      const contentSubject = await getSubject(code)
+      const desc = contentSubject[0].descricao_glossario
+      const norma = contentSubject[0].norma
+      if (desc || norma) {
+        const button = document.createElement("button")
+        button.innerHTML = `<sup>${icon}</sup>`
+
+        button.addEventListener("click", () => {
+          const div = document.createElement("span");
+          if (desc) {
+            div.innerHTML += `<div><span style="font-weight: 600; font-size:20px; margin: 10px 0; display:inline-block;">Explicação: </span><br />${desc}</div>`
+          }
+          if (norma) {
+            div.innerHTML += `<div><span style="font-weight: 600; font-size:20px; margin: 10px 0; display:inline-block;">Norma: </span><br />${norma}</div>`
+          }
+          showSheet(div)
+        })
+
+        div.append(button)
+      }
+    }
+    containerSubject.appendChild(div)
+  }));
+}
